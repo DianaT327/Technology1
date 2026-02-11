54ea6580cfb20556eab3e04805860af97897d88b
DianaT327 <didiana.taranenko@gmail.com>
2026-02-11 13:09:24 +0300

    Add array length check

--- example/array.py
+++ example/array.py
@@ -1,14 +1,17 @@
 arr = list()
 n = int(input("Enter number of elements: "))
-print("Enter elements:")
-i = 0
-while (i < n):
-    tmp = int(input(""))
-    arr.append(tmp)
-    i += 1
-print("Array:")
-i = 0
-while (i < n):
-    print(arr[i], end = " ")
-    i += 1
-print("")
+if (n <= 15 and n > 0):
+    print("Enter elements:")
+    i = 0
+    while (i < n):
+        tmp = int(input(""))
+        arr.append(tmp)
+        i += 1
+    print("Array:")
+    i = 0
+    while (i < n):
+        print(arr[i], end=" ")
+        i += 1
+        print("")
+else:
+    print("There should be no more than 15 elements")
