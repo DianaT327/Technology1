44029e0f09e2bec50c24a7af28eb92cd5d3e2205
LizaA <adalizik@gmail.com>
2026-02-11 13:06:37 +0300

    add input and output functions

--- example/array.py
+++ example/array.py
@@ -1,14 +1,19 @@
-arr = list()
-n = int(input("Enter number of elements: "))
-print("Enter elements:")
-i = 0
-while (i < n):
-    tmp = int(input(""))
-    arr.append(tmp)
-    i += 1
-print("Array:")
-i = 0
-while (i < n):
-    print(arr[i], end = " ")
-    i += 1
-print("")
+def vyvod (arr,n):
+    print("Array:")
+    i = 0
+    while (i < n):
+        print(arr[i], end=" ")
+        i += 1
+    print("")
+def vvod():
+    arr = list()
+    n = int(input("Enter number of elements: "))
+    print("Enter elements:")
+    i = 0
+    while (i < n):
+        tmp = int(input(""))
+        arr.append(tmp)
+        i += 1
+    return arr, n
+arr, n = vvod()
+vyvod(arr,n)
